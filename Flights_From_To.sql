1359ffa8dc163a3e19aef766e5855ef5be951fbe
jade-pang <whpangaa@connect.ust.hk>
2024-03-14 17:33:28 -0400

    created new queries

--- Flights_From_To.sql
+++ Flights_From_To.sql
@@ -1,10 +1,17 @@
--- View to show all flights from a specific source to destination
-CREATE VIEW Flights_From_To AS
+-- Query to find flights based on source and destination
 SELECT * FROM Flights
-WHERE Source = 'YourSource' AND Destination = 'YourDestination';
+WHERE Source = 'Toronto' AND Destination = 'New York';
 
--- Query to find the cheapest flight for a route
--- Replace 'YourSource' and 'YourDestination' with actual values
-SELECT * FROM Flights
-WHERE Source = 'YourSource' AND Destination = 'YourDestination'
-ORDER BY Price ASC;
+-- Query to calculate average price of flights for each airline
+SELECT Airline_ID, AVG(Price) AS Average_Price
+FROM Flights
+GROUP BY Airline_ID;
+
+-- Query to find total duration of flights for each airline
+SELECT Airline_ID, SUM(Duration) AS Total_Duration
+FROM Flights
+GROUP BY Airline_ID;
+
+-- Query to list all bookings with additional info
+SELECT Booking_ID, Flight_ID, Additional_Info
+FROM Bookings;
